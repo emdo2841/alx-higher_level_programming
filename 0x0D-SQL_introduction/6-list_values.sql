4756077d157e5f40bebc051eab70b2262993c015
emmanuel jonathan <emdo2841@gmail.com>
2023-03-15 19:21:51 -0700

    solutiont to introduction to Task

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+able File  5 lines (4 sloc)  225 Bytes
+
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
