55df8f5804703836cecedba2256695776854e02b
emdo2841 <114536437+emdo2841@users.noreply.github.com>
2023-09-06 14:10:39 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,7 +0,0 @@
-table File  5 lines (5 sloc)  195 Bytes
-
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
